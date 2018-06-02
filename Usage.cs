a7a2fafae303d1c85d3b736b1e3263c89bed9252
ForestDweller014 <39864689+ForestDweller014@users.noreply.github.com>
2018-06-02 17:29:36 -0400

    Update Usage.cs

--- Usage.cs
+++ Usage.cs
@@ -2,10 +2,10 @@ using System;
 
 public class Usage {
     public static void Main() {
-	      string[] parameters = new string[]{'Equidae','fast','plains','medium'};
-	      string[] animals = DBClass.Query("SELECT animals FROM animaldata WHERE (Family = @0 AND speed = @1 AND habitat = @2 AND size = @3)", parameters);
-	      foreach (string animal in animals) {
-	          Console.WriteLine(animal);
-	      }
-	  }
+        string[] parameters = new string[]{'Equidae','fast','plains','medium'};
+	string[] animals = DBClass.Query("SELECT animals FROM animaldata WHERE (Family = @0 AND speed = @1 AND habitat = @2 AND size = @3)", parameters);
+	foreach (string animal in animals) {
+	    Console.WriteLine(animal);
+	}
+    }
 }
